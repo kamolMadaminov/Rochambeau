f0c3acbb98f46b13e2d3211a527cd744258ba1c5
developerkamol <developerkamol@gmail.com>
2025-03-24 16:03:37 +0500

    Initial Commit

---
+++ Rochambeau/RochambeauApp.swift
@@ -0,0 +1,17 @@
+//
+//  RochambeauApp.swift
+//  Rochambeau
+//
+//  Created by Kamol Madaminov on 24/03/25.
+//
+
+import SwiftUI
+
+@main
+struct RochambeauApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
